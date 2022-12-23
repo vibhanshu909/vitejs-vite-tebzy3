f2512eabd52e202df0c1542237c10434484bdddb
vibhanshu909 <vibhanshu909@gmail.com>
2022-12-23 21:53:52 +0530

    2 files modified, 3 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import { useState } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom';
+
+const Home = () => (
+  <>
+    <a href="/login">Login</a>
+    <h1> Home</h1>
+  </>
+);
+const Login = () => <h1> Login</h1>;
 
 const routes = createBrowserRouter([
   {
     index: true,
-    element: (
-      <>
-        <a href="/login">Login</a>
-        <h1> Home</h1>
-      </>
-    ),
+    element: <Home />,
   },
   {
     path: '/login',
-    element: <h1> Login</h1>,
+    element: <Login />,
   },
-  {},
 ]);
 
 function App() {
   return (
     <>
-      <RouterProvider router={routes} />
+      {/* <RouterProvider router={routes} /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
